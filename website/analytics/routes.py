fc555257e6c4590a0936b4abbe04ea97c471e129
Evan Beese <egbeesefl@gmail.com>
2019-02-03 15:40:58 -0600

    worker fixes

--- website/analytics/routes.py
+++ website/analytics/routes.py
@@ -8,6 +8,8 @@ from rq import Queue
 import secrets
 from PIL import Image
 import os
+from cloudinary.uploader import upload
+from cloudinary.utils import cloudinary_url
 
 analytics_bp = Blueprint('analytics', __name__)
 
@@ -20,15 +22,18 @@ def analytics():
         print(form)
         if form.picture.data:
             img=form.picture.data
+            upload_result = upload(img)
+            thumbnail_url1, options = cloudinary_url(upload_result['public_id'], format='jpg', crop='fill', width=299, height=299)
             random_hex = secrets.token_hex(8)
             _, f_ext = os.path.splitext(img.filename)
             picture_fname = f'{random_hex}{f_ext}'
-            picture_path = os.path.join(url_for('static', filename=f'uploads/{picture_fname}'))
+            picture_path = os.path.join(current_app.root_path, 'static/uploads', picture_fname)
             output_size = (299, 299)
             i = Image.open(img)
             i.thumbnail(output_size)
             i.save(picture_path)
-            result = q.enqueue_call(func=prepare_img, args=([picture_path]), result_ttl=600)
+
+            result = q.enqueue_call(func=prepare_img, args=([thumbnail_url1]), result_ttl=600)
             return redirect(f'/analytics/{result.get_id()}')
         # return jsonify(picture_file)
         return render_template('results.html', results=picture_file, form=form, picture_path=picture_path)
