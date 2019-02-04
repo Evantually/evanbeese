9be6a14172484f2988785e32c689b6557c56f296
Evan Beese <egbeesefl@gmail.com>
2019-02-04 10:15:38 -0600

    worker fixes

--- website/analytics/forms.py
+++ website/analytics/forms.py
@@ -4,5 +4,5 @@ from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length
 
 class ClassificationForm(FlaskForm):
-    picture = FileField('Submit a pet image for classification!', validators=[FileAllowed(['jpg', 'png'])])
+    picture = FileField('Submit a pet image for classification!', validators=[FileAllowed(['jpg', 'png', 'tif', 'gif'])])
     submit = SubmitField('Analyze Image')
