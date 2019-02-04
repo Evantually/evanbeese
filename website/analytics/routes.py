416c1a9a5adef0a3e9b6b7d455c4ece1062ca634
Evan Beese <egbeesefl@gmail.com>
2019-02-03 23:28:10 -0600

    worker fixes

--- website/analytics/routes.py
+++ website/analytics/routes.py
@@ -39,7 +39,7 @@ def analytics_response(jobID):
         return 'Currently processing image. Please wait!', 202
     if job.is_failed:
         return str('Sorry. We experienced an error when processing your image.')
-    print(job.result[0])
+    print(job.result[0]['predictions'])
     return render_template('results.html', form=form, results=jsonify(job.result[0]), picture_path=jsonify(job.result[1]))
 
 @analytics_bp.route('/analytics/<jobID>/done', methods=['GET'])
